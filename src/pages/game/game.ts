903de256339582728b0e82b9f58e2a6c23ca3015
Cliff Delsoin <cdelsoin@gmail.com>
2017-08-12 20:27:20 -0400

    fix scoring bug clean up code to use proper functions remove template assets

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -13,7 +13,7 @@ export class GamePage {
 
   game: Phaser.Game
   stage: Phaser.Stage
-  sprite: Phaser.Sprite
+  trappy: Phaser.Sprite
   city: Phaser.Sprite
   coin: Phaser.Sprite
   coins: any
@@ -34,73 +34,59 @@ export class GamePage {
   }
 
   create() {
+    // Change the background color of the game to blue
+    this.game.stage.backgroundColor = '#445669'
 
-      // Change the background color of the game to blue
-      this.game.stage.backgroundColor = '#445669'
+    // Set the physics system
+    this.game.physics.startSystem(Phaser.Physics.ARCADE)
 
-      // Set the physics system
-      this.game.physics.startSystem(Phaser.Physics.ARCADE)
+    // Display the different visual elements
+    // z-index is decided by order of load (Last to load = on top)
+    this.floor = this.game.add.tileSprite(0, window.innerHeight-82, 1500, 265, 'floor')
+    this.city = this.game.add.sprite(0, window.innerHeight-241, 'background')
+    this.trappy = this.game.add.sprite(100, 245, 'trappy')
 
-      // Display the different visual elements
-      // z-index is decided by order of load (Last to load = on top)
-      this.floor = this.game.add.tileSprite(0, window.innerHeight-82, 1500, 265, 'floor')
-      this.city = this.game.add.sprite(0, window.innerHeight-241, 'background')
-      this.sprite = this.game.add.sprite(100, 245, 'trappy')
+    // Resize Trappy
+    this.trappy.scale.x = 0.3
+    this.trappy.scale.y = 0.3
 
-      // Resize Trappy
-      this.sprite.scale.x = 0.3
-      this.sprite.scale.y = 0.3
+    // Resize city background to fit screen
+    this.city.scale.x = 1.05
 
-      // Resize city background to fit screen
-      this.city.scale.x = 1.05
+    // Make group for coins to sit in
+    this.coins = this.game.add.group()
 
-      // Make group for coins to sit in
-      this.coins = this.game.add.group()
+    // Create coins every 1.25 seconds by calling createCoins()
+    this.game.time.events.loop(1250, GamePage.prototype.createCoins, this)
 
-      // Create coins every 1.25 seconds
-      // Size coin and add velocity
-      this.game.time.events.loop(1250, function (this) {
-        // this chooses what range the coin can spawn in
-        this.coin = this.game.add.sprite(window.innerWidth, this.game.rnd.integerInRange(5, window.innerHeight-120), 'coin')
-        this.coins.add(this.coin)
-        this.game.physics.arcade.enable(this.coin)
-        this.coin.scale.x = 0.2
-        this.coin.scale.y = 0.2
-        this.coin.body.velocity.x = -150
+    // (EXAMPLE CODE) Display Trappy spritesheet animation
+    // this.trappy = this.game.add.sprite(100, 245, 'trappysheet')
+    // this.trappy.frame = 3
 
-        // Kill the coin if it leaves the bounds of the world
-        this.coin.checkWorldBounds = true
-        this.coin.outOfBoundsKill = true
-      }, this)
+    // Add physics to Trappy
+    // Needed for: movements, gravity, collisions, etc.
+    this.game.physics.arcade.enable(this.trappy)
 
-      // (EXAMPLE CODE) Display Trappy spritesheet animation
-      // this.sprite = this.game.add.sprite(100, 245, 'trappysheet')
-      // this.sprtie.frame = 3
+    // Add gravity to the Trappy to make it fall
+    this.trappy.body.gravity.y = 1000
 
-      // Add physics to Trappy
-      // Needed for: movements, gravity, collisions, etc.
-      this.game.physics.arcade.enable(this.sprite)
+    // Call the 'jump' function when screen is tapped
+    this.game.input.onTap.add(GamePage.prototype.jump, this)
 
-      // Add gravity to the Trappy to make it fall
-      this.sprite.body.gravity.y = 1000
+    // This is our score that goes up each time a coin is collected
+    // collection and counting happen in update()
+    this.scoreCounter = 0
+    this.scoreLabel = this.game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" })
 
-      // Call the 'jump' function when screen is tapped
-      this.game.input.onTap.add(GamePage.prototype.jump, this)
-
-      // This is our score that goes up each time a coin is collected
-      // collection and counting happen in update()
-      this.scoreCounter = 0
-      this.scoreLabel = this.game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" })
-
-      this.sprite.anchor.setTo(-0.2, 0.5);
+    this.trappy.anchor.setTo(-0.2, 0.5)
 
   }
 
   update() {
     // Reset Trappy's position if hits the floor or the ceiling
-    if (this.sprite.y > window.innerHeight-117) {
-      this.sprite.y = 245 // puts the sprite back at its starting point
-      this.sprite.body.velocity.y = 0 // slows sprite down to stop
+    if (this.trappy.y > window.innerHeight-117) {
+      this.trappy.y = 245 // puts the sprite back at its starting point
+      this.trappy.body.velocity.y = 0 // slows sprite down to stop
 
       this.scoreCounter = 0
       this.scoreLabel.text = this.scoreCounter
@@ -109,33 +95,54 @@ export class GamePage {
     // Add repeating floor animation
     this.floor.tilePosition.x -= 2
 
-    // When a Trappy collides with a coin
-    // +1 to score and kill the coin
-    this.game.physics.arcade.overlap( this.sprite, this.coins, function(this) {
-      var coin = this.coins.children[0]
-      this.coins.remove(coin)
-      this.scoreCounter += 1
-      this.scoreLabel.text = this.scoreCounter
-    }, null, this)
+    // Call updateScore when a Trappy overlaps with a coin
+    this.game.physics.arcade.overlap( this.trappy, this.coins, GamePage.prototype.updateScore, null, this)
 
-    if (this.sprite.angle < 20)
-    this.sprite.angle += 1;
+    if (this.trappy.angle < 30)
+    this.trappy.angle += 1
 
   }
 
   // Make Trappy jump
   jump() {
     // Add a vertical velocity to Trappy
-    this.sprite.body.velocity.y = -400
+    this.trappy.body.velocity.y = -400
 
-    // Create an animation on the bird
-    var animation = this.game.add.tween(this.sprite);
+    // Create an animation on Trappy
+    var animation = this.game.add.tween(this.trappy)
 
-    // Change the angle of the bird to -20° in 100 milliseconds
-    animation.to({angle: -40}, 100);
+    // Change the angle of Trappy to -30° in 100 milliseconds
+    animation.to({angle: -30}, 100)
 
     // And start the animation
-    animation.start();
+    animation.start()
+  }
+
+  createCoins () {
+    // this chooses what range the coin can spawn in
+    this.coin = this.game.add.sprite(window.innerWidth, this.game.rnd.integerInRange(5, window.innerHeight-120), 'coin')
+    this.coins.add(this.coin)
+    this.game.physics.arcade.enable(this.coin)
+
+    // Size coin and add velocity
+    this.coin.scale.x = 0.2
+    this.coin.scale.y = 0.2
+    this.coin.body.velocity.x = -150
+
+    // Kill the coin if it leaves the bounds of the world
+    this.coin.checkWorldBounds = true
+    this.coin.outOfBoundsKill = true
+  }
+
+  updateScore (trappy, coins) {
+    // if the coin is already dead then don't do anything
+    if (!coins.alive) {
+      return;
+    }
+    // Okay, it is alive, so kill it and increment the score!
+    coins.kill()
+    this.scoreCounter += 1
+    this.scoreLabel.text = this.scoreCounter
   }
 
   // Restart the game
