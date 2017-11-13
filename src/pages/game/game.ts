bef7f865b7b2892f11fd601c4fce9e53072d373b
Cliff Delsoin <cdelsoin@gmail.com>
2017-11-12 23:25:25 -0500

    creat howto page (default for start button for test)

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -16,7 +16,7 @@ export class GamePage extends Phaser.Game {
 
       this.state.add('Start', Start, false);
       this.state.add('Play', Play, false);
-      // this.state.add('Over', Over, false);
+      this.state.add('How', How, false);
 
       this.state.start('Start')
   }
@@ -29,9 +29,9 @@ export class Start extends Phaser.State {
   stage: Phaser.Stage
   trappy: Phaser.Sprite
   city: any
+  floor: any
   banner: Phaser.Sprite
   button: any
-  floor: any
 
   preload() {
 
@@ -99,7 +99,7 @@ export class Start extends Phaser.State {
   }
 
   startGame() {
-    this.game.state.start('Play', true, false);
+    this.game.state.start('How', true, false);
   }
 }
 
@@ -342,4 +342,103 @@ export class Play extends Phaser.State {
     this.tryButton.scale.y = 0.4
     this.tryButton.anchor.setTo(0.5, 0.5)
   }
+}
+
+
+export class How extends Phaser.State {
+
+  game: Phaser.Game
+  stage: Phaser.Stage
+  trappy: Phaser.Sprite
+  coin: Phaser.Sprite
+  arrow: Phaser.Sprite
+  city: any
+  floor: any
+  trappyHowTo: any
+  coinHowTo: any
+  arrowHowTo: any
+
+
+  preload() {
+    this.game.load.image('background', 'assets/stage/background-night-2x.png')
+    this.game.load.image('floor', 'assets/stage/floor-night.png')
+    this.game.load.image('coin', 'assets/sprite/coin.png')
+
+    this.game.load.spritesheet('trappysheet', 'assets/sprite/trappy-spritesheet.png', 162, 174)
+    this.game.load.spritesheet('arrowsheet', 'assets/sprite/arrow-spritesheet.png', 189, 72)
+  }
+
+  create() {
+    // Change the background color of the game to blue
+    this.game.stage.backgroundColor = '#445669'
+
+    // Set the physics system
+    this.game.physics.startSystem(Phaser.Physics.ARCADE)
+
+    // Display the different visual elements
+    // z-index is decided by order of load (Last to load = on top)
+    this.city = this.game.add.tileSprite(0, window.innerHeight-225, 1500, 510,'background')
+    this.floor = this.game.add.tileSprite(0, window.innerHeight-82, 1500, 265, 'floor')
+    // this.banner = this.game.add.sprite(this.game.world.centerX, window.innerHeight-550, 'banner')
+    // this.button = this.game.add.button(this.game.world.centerX, window.innerHeight-300, 'button', Start.prototype.startGame)
+
+    // add the Trappy Spritesheet
+    this.trappy = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY-250, 'trappysheet')
+    // name the animation
+    this.trappy.animations.add('flap')
+
+    // play the animation .play(name, fps, loop?)
+    this.trappy.animations.play('flap', 10, true)
+
+    // Resize Trappy
+    this.trappy.scale.x = 0.4
+    this.trappy.scale.y = 0.4
+
+    // Resize city background to fit screen
+    this.city.scale.x = 0.3
+    this.city.scale.y = 0.3
+
+    // this.button.scale.x = 0.4
+    // this.button.scale.y = 0.4
+
+    this.trappy.anchor.setTo(0.5, 0.5)
+    // this.button.anchor.setTo(0.5, 0.5)
+
+    this.arrow = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY+10, 'arrowsheet')
+    this.arrow.anchor.setTo(0.5, 0.5)
+    this.arrow.animations.add('shoot')
+
+    // play the animation .play(name, fps, loop?)
+    this.arrow.animations.play('shoot', 7, true)
+
+    // Size arrow
+    this.arrow.scale.x = 0.4
+    this.arrow.scale.y = 0.4
+
+    this.coin = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY-110, 'coin')
+
+    this.coin.anchor.setTo(0.5, 0.5)
+
+    // Size coin
+    this.coin.scale.x = 0.4
+    this.coin.scale.y = 0.4
+
+    this.trappyHowTo = this.game.add.text(this.game.world.centerX, this.game.world.centerY-190, "Tap the screen to Flap Trappy", { font: "18px Arial", fill: "#ffffff" })
+    this.trappyHowTo.anchor.setTo(0.5, 0.5)
+    this.coinHowTo = this.game.add.text(this.game.world.centerX, this.game.world.centerY-60, "Get your money up. Collect coins.", { font: "18px Arial", fill: "#ffffff" })
+    this.coinHowTo.anchor.setTo(0.5, 0.5)
+    this.arrowHowTo = this.game.add.text(this.game.world.centerX, this.game.world.centerY+55, "These guys kill you... avoid them", { font: "18px Arial", fill: "#ffffff" })
+    this.arrowHowTo.anchor.setTo(0.5, 0.5)
+  }
+
+  update() {
+    // Add repeating floor & city animation
+    this.floor.tilePosition.x -= 2
+    this.city.tilePosition.x -= 1.3
+  }
+
+  goHome() {
+    this.game.state.start('Start', true, false);
+  }
+
 }
